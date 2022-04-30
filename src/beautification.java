ffbb64bc6932f450be9c4a1bffca10dfb04d7506
Harshit Raheja <quantumbytestudios@gmail.com>
2022-05-01 00:04:32 +0530

    Seperated Directory For Source

---
+++ src/beautification.java
@@ -0,0 +1,9 @@
+public class beautification {
+  public static void delayTextEffect(String text) {
+    for (int i = 0; i < text.length(); i++) {
+      char printIt = text.charAt(i);
+      System.out.print(printIt);
+      utilities.sleep(100);
+    }
+  }
+}
