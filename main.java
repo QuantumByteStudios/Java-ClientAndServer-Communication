48b8e85e6619db83e1eee65e9b76b1f3413ef905
Harshit Raheja <quantumbytestudios@gmail.com>
2022-05-01 00:05:23 +0530

    Updated: Main

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 import java.io.*;
+import java.security.DrbgParameters.NextBytes;
 import java.util.Scanner;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import javax.swing.text.Utilities;
 
 // Main Class
 public class main {
@@ -22,12 +26,38 @@ public class main {
     // System.out.println("Java Console Chat App"); //Basic
     // systemUtilities.sleep(500);
     beautyElement
-        .delayTextEffect("Welcome to " + colorConsole.RED + "Java Console Chat Application\n" + colorConsole.RESET);
+        .delayTextEffect(
+            "Welcome to " + colorConsole.RED + "Java-ClientAndServer-Communication\n" + colorConsole.RESET);
     beautyElement.delayTextEffect(
         colorConsole.YELLOW + "Please Enter your " + colorConsole.GREEN + "Name: ");
     userName = getInput.nextLine();
 
     ConsoleColors.RESETSCREEN();
+
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    LocalDateTime now = LocalDateTime.now();
+
+    try {
+      PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("logs.txt", true)));
+      out.println(dtf.format(now) + " Login: " + userName);
+      out.close();
+    } catch (IOException e) {
+    }
+
+    System.out.print("\nHey, " + colorConsole.RED + userName + colorConsole.RESET
+        + " What You Want To Execute: \n--> 1) Server\n--> 2) Client\n\nYour Input: ");
+    int choice = getInput.nextInt();
+    switch (choice) {
+      case 1:
+        currentInstance.systemInvoke("java Server");
+        break;
+      case 2:
+        currentInstance.systemInvoke("java Client");
+        break;
+      default:
+        System.out.println(colorConsole.RED + "Error: Invalid Option" + colorConsole.RESET);
+        break;
+    }
 
     // Destruction Section :P
     getInput.close();
