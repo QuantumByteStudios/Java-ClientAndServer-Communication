3577e89c225e31b5c94e1107c3af2edc82f09138
Harshit <quantumbytestudios@gmail.com>
2022-05-05 22:26:53 +0530

    Update

--- makeJarFile.bat
+++ makeJarFile.bat
@@ -1,3 +1,4 @@
 @echo off
 echo.
 jar -cfvm main.jar manifest.txt main.class utilities.class Server.class Client.class ConsoleColors.class beautification.class
+jar -cfvm linux-JAR.jar manifest.txt main.class utilities.class Server.class Client.class ConsoleColors.class beautification.class
